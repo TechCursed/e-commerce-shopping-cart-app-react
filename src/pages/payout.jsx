47d501b65c7b7ed31b3d2159634d54e1c6708369
techcursed <31003460+TechCursed@users.noreply.github.com>
2023-06-27 20:02:37 +0530

    Added form validation
    implemented form validation in bootstrap forms.

--- src/pages/payout.jsx
+++ src/pages/payout.jsx
@@ -1,17 +1,42 @@
 import React from 'react'
 import { Form, Button, Row, Col } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
-
+import { useState } from 'react';
 const Payout = () => {
 
+ 
+ const [email, setEmail] = useState('');
+ const [address, setAddress] = useState('');
+ const [city, setCity] = useState('');
+ const [zip, setZip] = useState('');
+ const [pass, setPass] = useState('');
+
+
+ 
  const navigate = useNavigate();
 
+//  this purchase function only triggers when form is validated correctly
+ const purchase = () => {
+  console.log(email, address, city, zip) 
+  alert('thanks for purchase')
+  navigate("/");
+ }
+
  const clickfunc = (e) =>{
-  e.preventDefault();
-  alert("Thanks for the purchase")
-  navigate("/")
+  e.preventDefault();  
+  email.length < 5 ? alert('email not valid') :
+  pass.length < 6 ? alert('password is atleat 8 characters long') :
+  address.length < 5 ? alert('please enter correct address') :
+  city < 1 ? alert('please enter city') :
+  zip < 4 ? alert('please enter correct zip code') :
+  (
+    purchase()
+  )
+  
+
 
  }
+
  return (
  <div>
 
@@ -21,18 +46,31 @@ const Payout = () => {
       <Row className="mb-3">
         <Form.Group as={Col} controlId="formGridEmail">
           <Form.Label>Email</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control type="email" 
+           placeholder="Enter email"  
+           value={email} 
+           onChange={(e) => setEmail(e.target.value) }
+           />
         </Form.Group>
 
         <Form.Group as={Col} controlId="formGridPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control type="password" 
+          placeholder="Password" 
+          required 
+          value={pass}
+          onChange={(e) => setPass(e.target.value)}
+          />
         </Form.Group>
       </Row>
 
       <Form.Group className="mb-3" controlId="formGridAddress1">
         <Form.Label>Address</Form.Label>
-        <Form.Control placeholder="1234 Main St" />
+        <Form.Control placeholder="1234 Main St" 
+        value={address}
+        required 
+        onChange={(e) =>  setAddress(e.target.value)}
+        />
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formGridAddress2">
@@ -43,7 +81,11 @@ const Payout = () => {
       <Row className="mb-3">
         <Form.Group as={Col} controlId="formGridCity">
           <Form.Label>City</Form.Label>
-          <Form.Control />
+          <Form.Control 
+          placeholder='City'
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+          />
         </Form.Group>
 
         <Form.Group as={Col} controlId="formGridState">
@@ -56,7 +98,10 @@ const Payout = () => {
 
         <Form.Group as={Col} controlId="formGridZip">
           <Form.Label>Zip</Form.Label>
-          <Form.Control />
+          <Form.Control 
+          value={zip}
+          onChange={(e) => setZip(e.target.value)}
+          />
         </Form.Group>
       </Row>
 
